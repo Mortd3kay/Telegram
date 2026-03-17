c64cfb16ba886cb8049aff8d23ea40cf90297fb3
Arsenij Shpilevoj <mrtdkprojects@gmail.com>
2026-03-18 01:23:11 +0300

    add newContactActivity as fullscreen

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -7840,10 +7840,9 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                     fragmentSearchField.editText.setText(str);
                     fragmentSearchField.editText.setSelection(str.length());
                 } else if (!str.equals("section")) {
-                    NewContactBottomSheet activity = new NewContactBottomSheet(DialogsActivity.this, getContext());
+                    NewContactActivity activity = new NewContactActivity();
                     activity.setInitialPhoneNumber(str, true);
-//                    presentFragment(activity);
-                    activity.show();
+                    presentFragment(activity);
                 }
             } else if (obj instanceof ContactsController.Contact) {
                 ContactsController.Contact contact = (ContactsController.Contact) obj;
